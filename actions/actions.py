d63f703d369f369e6c7526c866d38a32ed93218f
neha-in-nevrio <128684317+neha-in-nevrio@users.noreply.github.com>
2023-03-28 09:16:02 +0530

    Add gitignore and Form actions

--- actions/actions.py
+++ actions/actions.py
@@ -8,10 +8,11 @@
 # This is a simple example for a custom action which utters "Hello World!"
 
 from typing import Any, Text, Dict, List
-
-from rasa_sdk import Action, Tracker
+from rasa_sdk import Action, Tracker, FormValidationAction
 from rasa_sdk.executor import CollectingDispatcher
-from rasa_sdk.forms import FormAction
+from rasa_sdk.types import DomainDict
+import phonenumbers
+import re
 
 
 class ActionHelloWorld(Action):
@@ -30,20 +31,74 @@ class ActionHelloWorld(Action):
 
         return []
     
-class ActionFormInfo(FormAction):
+
+def clean_name(name):
+    return "".join([c for c in name if c.isalpha()])    
+
+class ValidateInfoForm(FormValidationAction):
 
     def name(self) -> Text:
-        """Unique identifier of the form"""
 
-        return "form_info"
+        return "validate_info_form"
     
-    @staticmethod
-    def required_slots(tracker: Tracker) -> List[Text]:
-        """A list of required slots that the form has to fill"""
 
-        return ["name", "phone_number", "email_id"]
+    def validate_full_name(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        """Validate 'full_name' value."""
+        
+        name = clean_name(slot_value)
+        if len(name) == 0:
+            dispatcher.utter_message(text= "That must've been a typo.")
+            return {"full_name": None}
+        return {"full_name": name}
+    
+    
+    
+    def validate_email_id(self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        
+        # full_name = tracker.get_slot("full_name")
 
-    def slot_mappings(self) -> Dict[Text, Any]:
-        return {}
+        email = slot_value
+
+        pat = "^[a-zA-Z0-9-_]+@[a-zA-Z0-9]+\.[a-z]{1,3}$"
+
+        if re.match(pat,email):
+            dispatcher.utter_message(text= f"Thank {full_name}")
+            return {"email_id": email}
+        return {"email_id": None}
+    
+    
+    def validate_phone_number(self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        """Validate 'phone_number' value."""
+
+        # full_name = tracker.get_slot("full_name")
+        # email_id = tracker.get_slot("email_id")
+
+    
+        if phonenumbers.parse(slot_value):
+            
+            number = slot_value
+            dispatcher.utter_message(text= f"Thank you {full_name} for your Email Id {email_id}.")
+            return {"phone_number": number}
+        
+        dispatcher.utter_message(text= f"Please enter correct phone number.")
+        return {"phone_number": None}
+        
+
 
         
