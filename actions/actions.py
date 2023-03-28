e4f875329ea6333a4b2fc7ae709b699a512dc2a8
neha-in-nevrio <128684317+neha-in-nevrio@users.noreply.github.com>
2023-03-29 00:07:41 +0530

    feat:validating-name-input

--- actions/actions.py
+++ actions/actions.py
@@ -12,6 +12,7 @@ from rasa_sdk import Action, Tracker, FormValidationAction
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.types import DomainDict
 from rasa_sdk.events import EventType, SlotSet
+import re
 
 
 class ActionHelloWorld(Action):
@@ -30,24 +31,31 @@ class ActionHelloWorld(Action):
 
         return []
     
-class ValidateInfoForm(Action):
 
+class ValidateInfoForm(FormValidationAction):
     def name(self) -> Text:
-
         return "validate_info_form"
 
-    def run(
-        self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict
-    ) -> List[EventType]:
-        required_slots = ["full_name", "email_id", "phone_number"]
+    def validate_full_name(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        """Validate `first_name` value."""
 
-        for slot_name in required_slots:
-            if tracker.slots.get(slot_name) is None:
-                # The slot is not filled yet. Request the user to fill this slot next.
-                return [SlotSet("requested_slot", slot_name)]
+        # If the name is super short, it might be wrong.
+        pattern = r'^[a-zA-Z]+([-\'\s][a-zA-Z]+)*$'
+        name = slot_value
+        if re.match(pattern, name):
+            print("full_name:"+ name)
+            return {"full_name": name}
 
-        # All slots are filled.
-        return [SlotSet("requested_slot", None)]
+        dispatcher.utter_message(text=f"That's a very short name. I'm assuming you mis-spelled.")
+        print("full_name: None")
+        return {"full_name": None}
+    
 
 class ActionSubmit(Action):
     def name(self) -> Text:
